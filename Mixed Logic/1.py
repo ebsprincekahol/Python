21087e69464ff8feec3926dbc6fd71a90dfaf277
Prince Kahol <princekahol99@gmail.com>
2025-08-26 16:54:17 +0530

    Mixed_Logic Questions

---
+++ Mixed Logic/1.py
@@ -0,0 +1,10 @@
+# Q 41 Generate a multiplication table of a given number?
+# Q 42 Find factorial of a number?
+# Q 43 Generate Fibonacci series up to N terms?
+# Q 44 Check if a number is prime?
+# Q 45 Find all Prime numbers in a given range?
+# Q 46 Calculate the sum of digits of a number?
+# Q 47 Reverse a number?
+# Q 48 Check if a number is an Armstrong number?
+# Q 49 Find the greatest common divisor (GCD) of two numbers?
+# Q 50 Find the least common multiple(LCM) of two numbers? 
